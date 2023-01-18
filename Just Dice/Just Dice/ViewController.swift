094786d7cf9f0e18a24e7d71295bc8fd0cac46c2
6lueparr0t <6lueparr0t@gmail.com>
2023-01-18 15:54:14 +0900

    add project just dice

--- Just Dice/Just Dice/ViewController.swift
+++ Just Dice/Just Dice/ViewController.swift
@@ -9,11 +9,67 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    @IBOutlet weak var diceImageView3: UIImageView!
+    
+    @IBOutlet weak var btnLucky: UIButton!
+    @IBOutlet weak var btnSimple: UIButton!
+    @IBOutlet weak var btnChicken: UIButton!
+    
+    @IBOutlet weak var lbResult: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        btnLucky.titleLabel?.textAlignment = .center
+        btnSimple.titleLabel?.textAlignment = .center
+        btnChicken.titleLabel?.textAlignment = .center
+        
+        lbResult.text = String(0)
     }
-
-
+    
+    let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+    
+    var dice1 = 0
+    var dice2 = 0
+    var dice3 = 0
+    
+    @IBAction func luckyRoll(_ sender: Any) {
+        dice1 = Int.random(in: 0...1) == 0 ? 0:5
+        dice2 = Int.random(in: 0...1) == 0 ? 0:5
+        dice3 = Int.random(in: 0...1) == 0 ? 0:5
+        
+        diceImageView1.image = diceArray[dice1]
+        diceImageView2.image = diceArray[dice2]
+        diceImageView3.image = diceArray[dice3]
+        
+        lbResult.text = String(dice1+dice2+dice3+3)
+    }
+    
+    @IBAction func simpleRoll(_ sender: Any) {
+        dice1 = Int.random(in: 0...5)
+        dice2 = Int.random(in: 0...5)
+        dice3 = Int.random(in: 0...5)
+        
+        diceImageView1.image = diceArray[dice1]
+        diceImageView2.image = diceArray[dice2]
+        diceImageView3.image = diceArray[dice3]
+        
+        lbResult.text = String(dice1+dice2+dice3+3)
+    }
+    
+    @IBAction func chickenRoll(_ sender: Any) {
+        dice1 = Int.random(in: 1...3)
+        dice2 = Int.random(in: 1...3)
+        dice3 = Int.random(in: 1...3)
+        
+        diceImageView1.image = diceArray[dice1]
+        diceImageView2.image = diceArray[dice2]
+        diceImageView3.image = diceArray[dice3]
+        
+        lbResult.text = String(dice1+dice2+dice3+3)
+    }
+    
 }
 
